684ef89093366ca969fff0bd9b0c1a1c0148f148
Ines21eemi <ines.romain@eemi.com>
2024-09-26 14:31:25 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,4 +9,6 @@ st.write(user_input)
 #Image
 st.image("https://www.mercedes-benz-mag.fr/wp-content/uploads/2021/09/HP-img-1680x842-22_Hamilton-1600x630.png")
 #Sidbar
-st.sidbar.title ("Inès Romain")
+st.sidebar.title ("Ines Romain")
+#Video dans la sidebar
+st.sidebar.video("https://www.youtube.com/watch?v=lofb298wDFc")
