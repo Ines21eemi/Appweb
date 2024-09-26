3a662de76c118734880ef52755a86ef43b4559e7
Ines21eemi <ines.romain@eemi.com>
2024-09-26 15:41:47 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -5,8 +5,12 @@ st.title ("Dall-e 3")
 #champ de saisi
 user_input : st.text_input ("Open IA")
 st.write(user_input)
-recherche_input : ("Votre texte")
+
+recherche_input : st.sidebar.text_input("Clé")
 st.write(recherche_input)
+
+#sidebar
+st.sidebar 
 
 #clé
 from openai import OpenAI
