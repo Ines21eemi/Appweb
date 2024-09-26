7b9cf5dced7f47dfdd1d075100c7c73c82458f6d
Ines21eemi <ines.romain@eemi.com>
2024-09-26 14:19:50 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,4 +4,5 @@ st.title("Mon formulaire")
 #Texte
 st.write("Ceci est un formulaire de contact")
 #Champ de saisi
-st.text_input("Tapez votre texte : ")
+user_input = st.text_input("Tapez votre texte : ")
+st.write(user_input)
