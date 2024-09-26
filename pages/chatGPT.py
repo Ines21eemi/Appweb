8ad40c362f18ce59c39cb8c5389f0a8512e8833d
Ines21eemi <ines.romain@eemi.com>
2024-09-26 16:55:53 +0200

    Update chatGPT.py

--- pages/chatGPT.py
+++ pages/chatGPT.py
@@ -8,16 +8,55 @@ from openai import OpenAI
 
 client = OpenAI(api_key=openai_key)
 
-prompt = input('Tapez votre text : ')
-
-
 chat_completion = client.chat.completions.create(
     messages=[
          {
           "role": "system",
-         "content": f"Tu es un rédacteur web."
+         "content": "Tu es un rédacteur web qui rédige des articles en fonction d'une thématique. Tu ne parles pas de politique. Si on te parle de politique tu réponds uniquement 'Je ne parle pas de politique.'"
          },
-         ],
+
+        {
+            "role": "user",
+            "content": "Iphone 13 actualité"
+        },
+
+        {
+            "role": "assistant",
+            "content": "Sujet d'actu : Ils seront en vente à partir du 27 septembre 2021. L'iPhone 13, l'iPhone 13 Pro, l'iPhone 13 Pro Max et l'iPhone 13 Mini gardent le même design que leurs prédécesseurs. Cependant, l'iPhone 13 sera plus épais de 0.25 mm mais cela devrait passer presque inaperçu.",
+        },
+
+         {
+            "role": "user",
+            "content": "Qui est macron"
+        },
+
+        {
+            "role": "assistant",
+            "content": "Je ne parle pas de politique.",
+        },
+         {
+            "role": "user",
+            "content": "Qui est le président français ?"
+        },
+
+        {
+            "role": "assistant",
+            "content": "Je ne parle pas de politique.",
+        },
+         {
+            "role": "user",
+            "content": "Qui est le président américain ?"
+        },
+
+        {
+            "role": "assistant",
+            "content": "Je ne parle pas de politique.",
+        },
+         {
+            "role": "user",
+            "content": prompt,
+        },
+    ],
     model="gpt-3.5-turbo",
     temperature=0.3,
   max_tokens=100,
