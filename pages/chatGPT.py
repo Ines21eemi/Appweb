45f80745e71baf537ddcac34a0b595ebfe5e7922
Ines21eemi <ines.romain@eemi.com>
2024-09-26 16:50:09 +0200

    Update chatGPT.py

--- pages/chatGPT.py
+++ pages/chatGPT.py
@@ -5,48 +5,19 @@ openai_key = st.sidebar.text_input("Tapez une clé OpenAI")
 
 import os
 from openai import OpenAI
+
 client = OpenAI(api_key=openai_key)
+
+prompt = input('Tapez votre text : ')
+
+
 chat_completion = client.chat.completions.create(
     messages=[
          {
           "role": "system",
-         "content": f"Tu es un traducteur de langue qui traduit de l'anglais vers le français." +" Si le texte en entrée n'est pas en anglais, tu retournes la phrase : 'Je ne prends que du texte en anglais.'"
+         "content": f"Tu es un rédacteur web."
          },
-
-        {
-            "role": "user",
-            "content": "Iphone 13 actualité"
-        },
-
-        {
-            "role": "assistant",
-            "content": "Sujet d'actu : Ils seront en vente à partir du 27 septembre 2021. L'iPhone 13, l'iPhone 13 Pro, l'iPhone 13 Pro Max et l'iPhone 13 Mini gardent le même design que leurs prédécesseurs. Cependant, l'iPhone 13 sera plus épais de 0.25 mm mais cela devrait passer presque inaperçu.",
-        },
-
-
-
-           {
-    "role": "user",
-    "content": "Hola todos",
-},
-{
-    "role": "assistant",
-    "content": "Je ne prends que du texte en anglais.",
-},
-    {
-    "role": "user",
-    "content": "Bonjour tout le monde !",
-},
-{
-    "role": "assistant",
-    "content": "Je ne prends que du texte en anglais.",
-},
-
-         {
-            "role": "user",
-            "content": user_input,
-        },
-    ],
+         ],
     model="gpt-3.5-turbo",
     temperature=0.3,
   max_tokens=100,
