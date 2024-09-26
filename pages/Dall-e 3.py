a1eb0666c7360bb97c16b62def9d2d07ae2aa612
Ines21eemi <ines.romain@eemi.com>
2024-09-26 15:33:19 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -2,4 +2,17 @@ import streamlit as st
 st.title ("Dall-e 3")
 user_input : st.text_input ("Open IA")
 st.write(user_input)
-sidebar_input : 
+recherche_input : ("Votre texte")
+st.write(recherche_input)
+from openai import OpenAI
+client = OpenAI(api_key=OpenAIKEY)
+prompt = "A cute baby sea otter"
+image = client.images.generate(
+    model="dall-e-2",
+    prompt=prompt,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+image_url = image.data[0].url
+print(image_url)
