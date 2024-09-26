252e91bc2b38579cdf6ca0415fd3acb93020eb01
Ines21eemi <ines.romain@eemi.com>
2024-09-26 14:21:32 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,3 +6,5 @@ st.write("Ceci est un formulaire de contact")
 #Champ de saisi
 user_input = st.text_input("Tapez votre texte : ")
 st.write(user_input)
+#Image
+st.image("https://www.mercedes-benz-mag.fr/wp-content/uploads/2021/09/HP-img-1680x842-22_Hamilton-1600x630.png")
